65175e713c322ef43661909af99b8751d51aa761
igorlangoni <igorlangoni@gmail.com>
2023-10-05 11:26:13 -0300

    refactored the app file

--- shop_manager/app.py
+++ shop_manager/app.py
@@ -14,43 +14,63 @@ class Application:
     
     def run(self):
         # Retrieve all items/orders
-        items_repository = ItemRepository(self.connection)
-        items = items_repository.all_items()
-        orders_repository = OrderRepository(self.connection)
-        orders = orders_repository.all_orders()
+        # items_repository = ItemRepository(self.connection)
+        # items = items_repository.all_items()
+        # orders_repository = OrderRepository(self.connection)
+        # orders = orders_repository.all_orders()
         
-        # asks the user for some input
-        print('Welcome to LOJA DO GALO!\n\n')
-        print('What do you want to do?\n')
-        print("1 - See list of items\n")
-        print("2 - See list of orders\n")
-        print("3 - Create item\n")
-        print("4 - Create order\n")
-        answer = int(input("Enter your choice: "))
-        print('\n')
-
-        if answer == 1:
-            for item in items:
-                print(f"{item.id} - {item.name} - Unit Price: {item.price}£ - In Stock: {item.quantity}")
+        while True:
             
-        if answer == 2:
-            for order in orders:
-                print(f"{order.id} - Customer: {order.customer} - Date: {order.date} - Item: {order.item}")
-        
-        if answer == 3:
-            item_name = input('Enter item name: ')
-            item_price = int(input('Enter item unit price: '))
-            item_quant = int(input('Enter item available quantity: '))
-            new_item = Item(None, item_name, item_price, item_quant)
-            items_repository.create_item(new_item)
-            print(f"{new_item} - Item added succesfully")
-        
-        if answer == 4:
-            order_customer = input('Enter customer name: ')
-            order_item = input('Enter item: ')
-            new_order = Order(None, order_customer, None, order_item)
-            orders_repository.create_order(new_order)
-            print(f"{new_order} - Order added succesfully")
+            items_repository = ItemRepository(self.connection)
+            items = items_repository.all_items()
+            orders_repository = OrderRepository(self.connection)
+            orders = orders_repository.all_orders()
+            
+            # asks the user for some input
+            print('Welcome to LOJA DO GALO!\n\n')
+            print('What do you want to do?\n')
+            print("1 - See list of items\n")
+            print("2 - See list of orders\n")
+            print("3 - Create item\n")
+            print("4 - Create order\n")
+            answer = int(input("Enter your choice: "))
+            print('\n')
+            
+
+            if answer == 1:
+                for item in items:
+                    print(f"{item.id} - {item.name} - Unit Price: {item.price}£ - In Stock: {item.quantity}")
+                
+            if answer == 2:
+                for order in orders:
+                    print(f"{order.id} - Customer: {order.customer} - Date: {order.date} - Item: {order.item}")
+                
+            if answer == 3:
+                item_name = input('Enter item name: ')
+                item_price = int(input('Enter item unit price: '))
+                item_quant = int(input('Enter item available quantity: '))
+                new_item = Item(None, item_name, item_price, item_quant)
+                items_repository.create_item(new_item)
+                print(f"{new_item} - Item added succesfully")
+            
+            if answer == 4:
+                order_customer = input('Enter customer name: ')
+                order_item = input('Enter item: ')
+                
+                for item in items:
+                    c = 0
+                    if item.name == order_item:
+                        new_order = Order(None, order_customer, None, order_item)
+                        orders_repository.create_order(new_order)
+                        print(f"{new_order} - Order added succesfully")
+                        c +=1
+                        break
+                if c == 0:
+                    print("Item not available")
+                        
+            cont = input("Wish to continue? y/n")
+            if cont == 'n':
+                    return False
 
 if __name__ == '__main__':
     app = Application()
