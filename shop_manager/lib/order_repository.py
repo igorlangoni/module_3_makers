ad02ef7c4c523d6c37de29a640a4e709161b2de2
igorlangoni <igorlangoni@gmail.com>
2023-10-05 11:25:51 -0300

    implemented the create order and reflected on stock

--- shop_manager/lib/order_repository.py
+++ shop_manager/lib/order_repository.py
@@ -1,4 +1,6 @@
 from lib.order import Order
+from lib.item import Item
+from lib. item_repository import ItemRepository
 
 class OrderRepository:
     def __init__(self, connection):
@@ -16,6 +18,15 @@ class OrderRepository:
     
     def create_order(self, order):
         self.connection.execute(
-            "INSERT INTO orders (customer, item) VALUES ('José', 'Football Boots, various sizes, adidas')"
+            "INSERT INTO orders (customer, item) VALUES (%s, %s)",
+            [order.customer, order.item]
         )
+        # TRYING TO REDUCE ITEM QUANTITY
+        repo = ItemRepository(self.connection)
+        items = repo.all_items()
+        for item in items:
+            if item.name == order.item:
+                self.connection.execute(
+                    "UPDATE items SET quantity = %s WHERE items.name = %s", [item.quantity-1, item.name]
+                )
         return None
